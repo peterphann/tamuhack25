bffd8081c63dd206454faaabb357d230286b575c
Sophia Phu <sophiatiffphu@gmail.com>
2025-01-26 09:46:38 -0600

    adjusted opacity woo hoo :)

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
     <div className={cn("flex flex-col mx-28 mt-48 mb-32 gap-y-32", afacad.className)}>
       <div className="flex justify-between items-center gap-x-12">
         <div className="flex flex-col gap-y-2 w-1/3 relative">
-          <RiCalendarTodoFill className="w-80 h-80 -z-50 opacity-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+          <RiCalendarTodoFill className="w-80 h-80 -z-50 opacity-[0.03] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
 
           <h3 className="text-2xl font-bold">Itinerary Planning</h3>
           <h4 className="text-4xl font-bold">Plan out your itinerary</h4>
@@ -69,7 +69,7 @@ export default function Home() {
         <Image className="w-2/3 object-contain shadow-lg rounded-md hover:-translate-y-2 duration-300" src={"/hotels.png"} alt="hotels" width="1920" height="1080" />
 
         <div className="flex flex-col gap-y-2 w-1/3 relative">
-          <RiHotelBedFill className="w-80 h-80 -z-50 opacity-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+          <RiHotelBedFill className="w-80 h-80 -z-50 opacity-[0.03] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
           <h3 className="text-2xl font-bold">Hotel Vouchers</h3>
           <h4 className="text-4xl font-bold">View and accept hotel vouchers</h4>
           <p className="text-xl">Flock provides a convenient way to redeem vouchers from any delayed/cancelled flight and send the money to your bank account.</p>
@@ -78,7 +78,7 @@ export default function Home() {
 
       <div className="flex justify-between items-center gap-x-12">
         <div className="flex flex-col gap-y-2 w-1/3 relative">
-          <RiCarFill className="w-80 h-80 -z-50 opacity-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+          <RiCarFill className="w-80 h-80 -z-50 opacity-[0.03] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
           <h3 className="text-2xl font-bold">Car Rentals</h3>
           <h4 className="text-4xl font-bold">Find and manage car rentals</h4>
           <p className="text-xl">We know that it can be challenging to find transportation in a new city. Flock’s car rental management makes it easy to get the transportation you need.</p>
